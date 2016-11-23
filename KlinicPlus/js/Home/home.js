72a4303205d5edf8c072c48db33e8755ce677893
w4sii <khatri.wasiq@gmail.com>
2016-11-23 18:00:20 +0500

    home.js, index.cshtml updated ResetPassword Popup window...

--- KlinicPlus/js/Home/home.js
+++ KlinicPlus/js/Home/home.js
@@ -129,4 +129,32 @@ $("#btnRegister").click(function () {
                             $("#city").focus();
                             return false;
                         }
-}); 
+});
+
+
+
+$("#forgot").click(function () {
+    var id = '#dialog';
+    //Get the screen height and width
+    var maskHeight = "  100%";
+    var maskWidth = "100%";
+
+    //Set heigth and width to mask to fill up the whole screen
+    $('#mask').css({ 'width': maskWidth, 'height': maskHeight });
+
+    //transition effect
+    $('#mask').fadeIn(500);
+    $('#mask').fadeTo("slow", 0.9);
+
+    //Get the window height and width
+    var winH = $(window).height();
+    var winW = $(window).width();
+
+    //Set the popup window to center
+    $(id).css('top', winH / 2 - $(id).height() / 2);
+    $(id).css('left', winW / 2 - $(id).width() / 2);
+
+    //transition effect
+    $(id).fadeIn(2000);
+    $(id).css("overflow", "hidden");
+});
