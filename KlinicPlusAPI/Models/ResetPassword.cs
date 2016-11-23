40f3c859b94758777b1e5b925a03e21bdc4706aa
w4sii <khatri.wasiq@gmail.com>
2016-11-23 22:32:49 +0500

    ResetPassword.cs Addedgit add -A ResetPassword.cs

---
+++ KlinicPlusAPI/Models/ResetPassword.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KlinicPlusAPI.Models
+{
+    public class ResetPassword
+    {
+        public string Email { get; set; }
+    }
+}
