25e4179fdea358a9684a7011363ad29db099e6e0
w4sii <khatri.wasiq@gmail.com>
2016-11-23 22:27:57 +0500

    ResetPasswordController added

---
+++ KlinicPlusAPI/Controllers/ResetPasswordController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Net.Mail;
+using KlinicPlusAPI.Models;
+
+namespace KlinicPlusAPI.Controllers
+{
+    public class ResetPasswordController : ApiController
+    {   // POST: api/ResetPassword
+        public void Post([FromBody] ResetPasword Email)
+        {
+
+        }
+    }
+}
